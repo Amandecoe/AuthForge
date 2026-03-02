98519c0e214fedb8892c01f3f122c3096bc2e5a3
Amanuel <amanuelbehailu557@gmail.com>
2026-03-02 17:39:16 +0300

    models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomeUser(AbstractUser):
+    is_verified = models.BooleanField(null = False, blank= False)
+    is_2FA_enabled = models.BooleanField(blank = True)
+    is_active = models.BooleanField()
+    ROLE_CHOICES = (
+        ('admin''Admin'),
+        ()
+    )
+    role = models.
