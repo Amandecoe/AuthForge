ce5a79ac2c633ef2d2e8a55515ac5edb9a50dac5
Amanuel <amanuelbehailu557@gmail.com>
2026-03-02 20:30:45 +0300

    password reset tokens table created

--- accounts/models.py
+++ accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
-class CustomeUser(AbstractUser):
+class CustomUser(AbstractUser):
     is_verified = models.BooleanField(null = False, blank= False)
     is_2FA_enabled = models.BooleanField(blank = True)
     is_active = models.BooleanField()
@@ -13,11 +13,15 @@ class CustomeUser(AbstractUser):
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='user')
 
 class email_verification_tokens(models.Model):
-    user_id = models.ForeignKey(CustomeUser, on_delete=models.CASCADE)
-    token = models.CharField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    token = models.CharField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True) #runs once when token is created
     expires_at = models.DateTimeField()
     is_used = models.BooleanField(blank=False)
 
 class password_reset_tokens(models.Model):
-    
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    token = models.CharField(blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField()
+    is_used = models.BooleanField(blank=False)
