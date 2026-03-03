b91aab140c2c374de3dfbca238ebc748ce9e093d
Amanuel <amanuelbehailu557@gmail.com>
2026-03-03 17:40:16 +0300

    signup not working

--- accounts/models.py
+++ accounts/models.py
@@ -3,14 +3,27 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    is_verified = models.BooleanField(default= False)
-    is_2FA_enabled = models.BooleanField(default = False)
-    is_active = models.BooleanField(default=True)
-    class rolechoices(models.TextChoices):
-     ADMIN = 'Admin'
-     USER = 'User'
-    role = models.CharField(max_length=10, choices=rolechoices, default='user')
+    email = models.EmailField(unique=True)
 
+    is_verified = models.BooleanField(default=False)
+    is_2FA_enabled = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    class RoleChoices(models.TextChoices):
+        ADMIN = "Admin", "Admin"
+        USER = "User", "User"
+
+    role = models.CharField(
+        max_length=10,
+        choices=RoleChoices.choices,
+        default=RoleChoices.USER
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']  
+
+    def __str__(self):
+        return self.email
 class email_verification_tokens(models.Model):
     user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     token = models.CharField(blank=False)
