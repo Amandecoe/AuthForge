a1053ec067950b334056e005edb226c4b5d53969
Amanuel <amanuelbehailu557@gmail.com>
2026-03-03 16:41:05 +0300

    admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import CustomUser
+from .forms import CustomUserCreationForm
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    model = CustomUser
+
+
+admin.site.register(CustomUser,CustomUserAdmin)
