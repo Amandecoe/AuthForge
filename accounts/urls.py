554a7b555aa4ead47647602735740d106b055ae7
Amanuel <amanuelbehailu557@gmail.com>
2026-03-04 12:54:09 +0300

    password change form done

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SignUpView, LoginView
+from .views import SignUpView, LoginView, PasswordChangeView
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name = "signup"),
-    path("login/", LoginView.as_view(), name = "login" )
+    path("login/", LoginView.as_view(), name = "login" ),
+    path("password_change/", PasswordChangeView.as_view(), name = "password_change"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from .forms import CustomUserCreationForm, CustomLoginView, PasswordChangeView
+from .forms import CustomUserCreationForm, CustomLoginView
 from .models import CustomUser
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordChangeView
 # Create your views here.
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
@@ -14,7 +14,6 @@ class LoginView(LoginView):
     template_name = "registration/login.html"
     authentication_form = CustomLoginView
 
-class PasswordChange(PasswordChangeView):
-    form_class = PasswordChangeView
-    template_name = "password_change_form.html"
+class PasswordChangeView(PasswordChangeView):
+    template_name = "registration/password_change_form.html"
     success_url = reverse_lazy("password_change_done")
