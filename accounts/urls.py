33c7d73ab61852ab687c33485eefdaf9b92abbcd
Amanuel <amanuelbehailu557@gmail.com>
2026-03-06 19:08:09 +0300

    password reset trial

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("signup/", SignUpView.as_view(), name = "signup"),
     path("login/", LoginView.as_view(), name = "login" ),
     path("password_change/", PasswordChangeView.as_view(), name = "password_change"),
+    path('reset/<uid64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"), name="password_reset_confirm")
 ]
