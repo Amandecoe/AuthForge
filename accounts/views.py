2b1f811d619c1331aa18e3a1c55d5ec58cd21dc8
Amanuel <amanuelbehailu557@gmail.com>
2026-03-06 06:17:59 +0300

    hover effect

--- accounts/views.py
+++ accounts/views.py
@@ -17,3 +17,5 @@ class LoginView(LoginView):
 class PasswordChangeView(PasswordChangeView):
     template_name = "registration/password_change_form.html"
     success_url = reverse_lazy("password_change_done")
+
+
