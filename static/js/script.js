4fd733deb64ee4c7b06a0468da8498af41debecd
Amanuel <amanuelbehailu557@gmail.com>
2026-03-09 08:29:16 +0300

    little left for the sidebar

--- static/js/script.js
+++ static/js/script.js
@@ -9,12 +9,6 @@ function toggleSidebar(){
         ul.previousElementSibling.classList.remove('rotate')
     })
 }
-function moveFirstToTop() {
-    const list = document.getElementById('start');
-    const firstLi = list.firstElementChild; // Gets the first <li> in the list
 
-    if (firstLi) {
-    // Moves the 'firstLi' element to be the first child of 'list'
-    list.prepend(firstLi);
-    }
-}
+
+
