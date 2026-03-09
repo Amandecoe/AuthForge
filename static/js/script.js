34773f83cc6023d88ecb54ae1a2899e7cec79544
Amanuel <amanuelbehailu557@gmail.com>
2026-03-09 07:56:19 +0300

    moving the top ones to top and the bottom ones bottom left

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
 const sidebar = document.getElementById('sidebar')
-
+const toggleButton = document.getElementById('toggle-btn')
 function toggleSidebar(){
     sidebar.classList.toggle('close')
     toggleButton.classList.toggle('rotate')
@@ -8,4 +8,13 @@ function toggleSidebar(){
         ul.classList.remove('show')
         ul.previousElementSibling.classList.remove('rotate')
     })
+}
+function moveFirstToTop() {
+    const list = document.getElementById('start');
+    const firstLi = list.firstElementChild; // Gets the first <li> in the list
+
+    if (firstLi) {
+    // Moves the 'firstLi' element to be the first child of 'list'
+    list.prepend(firstLi);
+    }
 }
