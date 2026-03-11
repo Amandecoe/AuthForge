685b4e30c98166fe58b1375bc25469058570cb41
Amanuel <amanuelbehailu557@gmail.com>
2026-03-11 07:59:19 +0300

    working on the table for users

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.views.generic.detail import DetailView
+from django.views.generic import ListView
 from django.contrib.auth import get_user_model
 from .models import UserProfile
 # Create your views here.
@@ -8,6 +8,10 @@ User = get_user_model()
 class HomePageView(TemplateView):
     template_name = "dashboard/home.html"
 
-class UserPageView(DetailView):
+class UserPageView(ListView):
     model = UserProfile
-    template_name = "dashboard/home.html"
+    template_name = "dashboard/users.html"
+    context_object_name = "users" #used to loop through the template
+
+    def get_queryset(self): #orders the users with date joined
+        return User.objects.all().order_by('-date_joined')
