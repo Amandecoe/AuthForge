8c18ef9eea5a2925af75abb14e866f77cff7e44c
Amanuel <amanuelbehailu557@gmail.com>
2026-03-11 07:41:45 +0300

    dasboard added to settings

--- AuthForge_Project/settings.py
+++ AuthForge_Project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "accounts",
     "crispy_forms",
+    "dashboard",
 ]
 
 MIDDLEWARE = [
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views.generic.detail import DetailView
+from django.contrib.auth import get_user_model
+from .models import UserProfile
 # Create your views here.
+User = get_user_model()
 class HomePageView(TemplateView):
     template_name = "dashboard/home.html"
 
-class UserPageView(TemplateView):
+class UserPageView(DetailView):
+    model = UserProfile
     template_name = "dashboard/home.html"
