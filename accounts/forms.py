7c9707593e1b83f63c11bc1cb404275fa7fce526
Amanuel <amanuelbehailu557@gmail.com>
2026-04-06 13:03:58 +0300

    login and signup done

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,29 +2,14 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .models import CustomUser
 from django import forms
 class CustomUserCreationForm(UserCreationForm):
-    first_name = forms.CharField(required=True)
-    last_name = forms.CharField(required=True)
+    full_name = forms.CharField(label = "full_name", max_length=255)
     class Meta:
         model = CustomUser
         fields = (
-            'first_name',
-            'last_name',
             'email',
             'password1',
             'password2',
-        )
-    #clean() checks if field is unique or not
-    def clean_first_name(self):
-        first_name = self.cleaned_data.get("first_name")
-        if not first_name:
-            raise forms.ValidationError("First Name is Required")
-        return first_name
-
-    def clean_last_name(self):
-        last_name = self.cleaned_data.get("last_name")
-        if not last_name:
-            raise forms.ValidationError("Last Name is Required")
-        return last_name
+        ) + ("full_name",)
 class CustomLoginView(AuthenticationForm):
     username = forms.EmailField(label = "Email", widget = forms.EmailInput(attrs = {
         "placeholder":"Enter Your Email"
