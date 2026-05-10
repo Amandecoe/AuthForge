9533666acfb0197fb86d5d7e606a6623c8a823fa
Amanuel <amanuelbehailu557@gmail.com>
2026-05-10 21:09:12 +0300

    user profile model created

--- dashboard/models.py
+++ dashboard/models.py
@@ -6,3 +6,8 @@ class UserProfile(models.Model):
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
+    profile_picture = models.ImageField(null=True, blank=True)
+    bio = models.TextField(max_length=200)
+    phone_number = models.PositiveSmallIntegerField(blank=True)
+    last_seen = models.DateTimeField(blank=True, null=True)
+    
