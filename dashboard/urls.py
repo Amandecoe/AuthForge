3eb7edc90ed43eed29d580c25b373133326eed4a
Amanuel <amanuelbehailu557@gmail.com>
2026-05-11 12:21:20 +0300

    profile loading correct but with no data

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,5 +5,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("home/",HomePageView.as_view(), name = "home"),
     path("users/",UserPageView.as_view(), name = "users"),
-    path ("profile/<int:pk>/", ProfilePageView.as_view(), name = "profile")
+    path ("profile/", ProfilePageView.as_view(), name = "profile")
 ]
