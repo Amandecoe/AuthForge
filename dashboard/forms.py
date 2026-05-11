b4d04b0bc15541a76a77b088ccf872918f790301
Amanuel <amanuelbehailu557@gmail.com>
2026-05-11 11:01:16 +0300

    change password form

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,10 +1,12 @@
 from .models import UserProfile
 from django import forms
 from django.forms import ModelForm
-
+from django.contrib.auth.forms import PasswordChangeForm
 class ProfileEditing(ModelForm):
     class Meta:
         model = UserProfile
         fields = ("full_name","profile_picture","username","email","bio","phone_number" )
 
-class ChangePassword()
+class ChangePassword(PasswordChangeForm):
+    class Meta:
+        model=UserProfile
