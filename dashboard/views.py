542966705f7bdbc9aee57452f48c2d3959656419
Amanuel <amanuelbehailu557@gmail.com>
2026-05-12 18:35:24 +0300

    account status view created

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,6 +8,10 @@ from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
 from django.contrib.auth.views import PasswordChangeView
 from django.urls import reverse_lazy
+from django.db.models import Count, Max
+from django.utils import timezone
+from datetime import timedelta
+from .models import Activity
 # Create your views here.
 User = get_user_model()
 class HomePageView(TemplateView):
@@ -47,3 +51,15 @@ def profile_data(request):
 class PasswordChangeView(PasswordChangeView):
     template_name = "registration/password_change_form.html"
     success_url = reverse_lazy("password_change_done")
+
+@login_required
+def account_status(request):
+    activity = Activity.objects.filter(
+        user = request.user,
+        action = "login",
+        )
+
+    stats = activity.aggregate(
+        login_count = Count("id"),
+        last_login = Max("timestamp")
+    )
